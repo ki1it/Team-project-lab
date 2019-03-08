ffb25ca5b1f18ee38afc1617172a9f13026a8b8c
kit <kirsh.ru@yandex.ru>
2019-03-08 07:58:48 +0700

    menu fix and add view for service lists

--- routes/servicelist.js
+++ routes/servicelist.js
@@ -1,16 +1,16 @@
 var express = require('express')
 var router = express.Router()
-
+const ServiceList = require('../database/models/ServiceList')
 router.get('/', async function (req, res, next) {
-  // let polomka = await ServiceListBreakdown.findAll({
-  //   where:{ServiceListFK:req.query.id},
-  //   include: [{ model: Breakdown, include: { model: BreakdownType, as: 'BreakdownType' },  as: 'Breakdown' }]
-  // })
-  //   .catch((err) => {
-  //     console.log(err)
-  //   })
+  let servicelist = await ServiceList.findAll({
+    // where:{ServiceListFK:req.query.id},
+    // include: [{ model: Breakdown, include: { model: BreakdownType, as: 'BreakdownType' },  as: 'Breakdown' }]
+  })
+    .catch((err) => {
+      console.log(err)
+    })
   res.render('servicelist', {
-
+    servicelist: servicelist,
   })
 })
 
