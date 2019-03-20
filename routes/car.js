7572533dd5325a33a61cdb29e9053ffc2e543ad4
kit <kirsh.ru@yandex.ru>
2019-03-20 15:53:16 +0700

    work with car

--- routes/car.js
+++ routes/car.js
@@ -4,15 +4,18 @@ const Car = require('../database/models/Car')
 const Brand = require('../database/models/Brand')
 const Model = require('../database/models/Model')
 const SprCar = require('../database/models/SprCar')
+const UrClient = require('../database/models/UrClient')
+const Client = require('../database/models/Client')
 router.get('/', async function (req, res, next) {
   let car = await Car.findAll({
     // where:{ServiceListFK:req.query.id},
-     include: [{ model: SprCar, include: [{ model: Brand, as: 'Brand' },{ model: Model, as: 'Model' }],  as: 'SprCar' }]
+     include: [{ model: SprCar, include: [{ model: Brand, as: 'Brand' },{ model: Model, as: 'Model' }],  as: 'SprCar' },{ model: Client, as: 'Client'},{ model: UrClient, as: 'UrClient'}]
   })
     .catch((err) => {
       console.log(err)
     })
   res.render('car', {
+
     car: car,
   })
 })
