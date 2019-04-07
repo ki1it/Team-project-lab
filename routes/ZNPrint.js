349d60f4f7d0a9b5558d526ecb6415cc662fcee2
kit <kirsh.ru@yandex.ru>
2019-04-07 11:00:06 +0700

    work with printing ZN

--- routes/ZNPrint.js
+++ routes/ZNPrint.js
@@ -2,6 +2,11 @@ var express = require('express')
 var router = express.Router()
 const ServiceList = require('../database/models/ServiceList')
 const ServiceList_Status = require('../database/models/ServiceList_Status')
+const ServiceList_Service = require('../database/models/ServiceList_Service')
+const ServiceList_Detail = require('../database/models/ServiceList_Detail')
+const Detail = require('../database/models/Detail')
+const Service = require('../database/models/Service')
+const EdIzmer = require('../database/models/EdIzmer')
 const Car = require('../database/models/Car')
 const SprCar = require('../database/models/SprCar')
 const UrClient = require('../database/models/UrClient')
@@ -18,6 +23,8 @@ router.get('/',async function (req, res, next) {
       {model: UrClient, as: 'UrClient'},
       {model: Car, as: 'Car', include: [{ model: SprCar, include: [{model: Brand, as: 'Brand'}, {model: Model, as: 'Model'}], as: 'SprCar'}]},
       {model: ServiceList_Status, as: 'ServiceList_Status'},
+      {model: ServiceList_Service, include: {model: Service, as: 'Service'}, as: 'ServiceList_Service'},
+      {model: ServiceList_Detail, include: {model: Detail, include: {model: EdIzmer, as: 'EdIzmer'}, as: 'Detail'}, as: 'ServiceList_Detail'},
     ],
     where:{id:req.query.id}
   })
