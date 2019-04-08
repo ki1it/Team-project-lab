1baae4e1666029b38c954acc5ea106c9c0b269b3
ki1it <kirsh.ru@yandex.ru>
2019-04-08 16:32:58 +0700

    improve search

--- routes/servicelist.js
+++ routes/servicelist.js
@@ -5,11 +5,19 @@ const ServiceList_Status = require('../database/models/ServiceList_Status')
 const Car = require('../database/models/Car')
 const UrClient = require('../database/models/UrClient')
 const Client = require('../database/models/Client')
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op
 router.get('/', async function (req, res, next) {
     let servicelist
+    let yesterday = new Date(new Date().setDate(new Date().getDate() - 1))
     if (req.query.searchid === undefined) {
         servicelist = await ServiceList.findAll({
-            // where:{ServiceListFK:req.query.id},
+            where: {
+                OpenDate:
+                    {
+                        [Op.gte]: yesterday.setHours(0,0,0,0)
+                    }
+            },
             //include: [{ model: Breakdown, include: { model: BreakdownType, as: 'BreakdownType' },  as: 'Breakdown' }]
 
             include: [{model: Client, as: 'Client'},
@@ -26,7 +34,7 @@ router.get('/', async function (req, res, next) {
             //where:{'Client.DLNumber':req.body.searchid},
             //include: [{ model: Breakdown, include: { model: BreakdownType, as: 'BreakdownType' },  as: 'Breakdown' }]
 
-            include: [{model: Client,where:{DLNumber:req.query.searchid}, as: 'Client'},
+            include: [{model: Client, where: {DLNumber: req.query.searchid}, as: 'Client'},
                 {model: UrClient, as: 'UrClient'},
                 {model: Car, as: 'Car'},
                 {model: ServiceList_Status, as: 'ServiceList_Status'},
@@ -35,15 +43,14 @@ router.get('/', async function (req, res, next) {
             .catch((err) => {
                 console.log(err)
             })
-        if(servicelist === undefined)
-        {
+        if (servicelist === undefined) {
             servicelist = await ServiceList.findAll({
                 //where:{Car.sequelize.literal('Car.VIN'):req.query.searchid},
                 //include: [{ model: Breakdown, include: { model: BreakdownType, as: 'BreakdownType' },  as: 'Breakdown' }]
 
                 include: [{model: Client, as: 'Client'},
                     {model: UrClient, as: 'UrClient'},
-                    {model: Car, where:{VIN:req.query.searchid}, as: 'Car'},
+                    {model: Car, where: {VIN: req.query.searchid}, as: 'Car'},
                     {model: ServiceList_Status, as: 'ServiceList_Status'},
                 ]
             })
@@ -51,14 +58,13 @@ router.get('/', async function (req, res, next) {
                     console.log(err)
                 })
         }
-        if(servicelist === undefined)
-        {
+        if (servicelist === undefined) {
             servicelist = await ServiceList.findAll({
                 //where:{Car.sequelize.literal('Car.VIN'):req.query.searchid},
                 //include: [{ model: Breakdown, include: { model: BreakdownType, as: 'BreakdownType' },  as: 'Breakdown' }]
 
                 include: [{model: Client, as: 'Client'},
-                    {model: UrClient, where:{INN:req.query.searchid}, as: 'UrClient'},
+                    {model: UrClient, where: {INN: req.query.searchid}, as: 'UrClient'},
                     {model: Car, as: 'Car'},
                     {model: ServiceList_Status, as: 'ServiceList_Status'},
                 ]
@@ -67,15 +73,14 @@ router.get('/', async function (req, res, next) {
                     console.log(err)
                 })
         }
-        if(servicelist === undefined)
-        {
+        if (servicelist === undefined) {
             servicelist = await ServiceList.findAll({
                 //where:{Car.sequelize.literal('Car.VIN'):req.query.searchid},
                 //include: [{ model: Breakdown, include: { model: BreakdownType, as: 'BreakdownType' },  as: 'Breakdown' }]
 
                 include: [{model: Client, as: 'Client'},
                     {model: UrClient, as: 'UrClient'},
-                    {model: Car, where:{GosNumber:req.query.searchid}, as: 'Car'},
+                    {model: Car, where: {GosNumber: req.query.searchid}, as: 'Car'},
                     {model: ServiceList_Status, as: 'ServiceList_Status'},
                 ]
             })
@@ -83,15 +88,29 @@ router.get('/', async function (req, res, next) {
                     console.log(err)
                 })
         }
-        if(servicelist === undefined)
-        {
+        if (servicelist === undefined) {
             servicelist = await ServiceList.findAll({
                 //where:{Car.sequelize.literal('Car.VIN'):req.query.searchid},
                 //include: [{ model: Breakdown, include: { model: BreakdownType, as: 'BreakdownType' },  as: 'Breakdown' }]
 
-                include: [{model: Client,where:{SecondName :req.query.searchid}, as: 'Client'},
+                include: [{model: Client, where: {SecondName: req.query.searchid}, as: 'Client'},
                     {model: UrClient, as: 'UrClient'},
-                    {model: Car,  as: 'Car'},
+                    {model: Car, as: 'Car'},
+                    {model: ServiceList_Status, as: 'ServiceList_Status'},
+                ]
+            })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
+        if (servicelist === undefined) {
+            servicelist = await ServiceList.findAll({
+                //where:{Car.sequelize.literal('Car.VIN'):req.query.searchid},
+                //include: [{ model: Breakdown, include: { model: BreakdownType, as: 'BreakdownType' },  as: 'Breakdown' }]
+
+                include: [{model: Client,  as: 'Client'},
+                    {model: UrClient,where: {Name: req.query.searchid}, as: 'UrClient'},
+                    {model: Car, as: 'Car'},
                     {model: ServiceList_Status, as: 'ServiceList_Status'},
                 ]
             })
